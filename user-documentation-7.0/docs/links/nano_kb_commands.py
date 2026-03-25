6bd6f41f1641bc19d8e8ad382adb424a07a572cd
r-paes <rpaes.0011@gmail.com>
2026-03-24 21:08:10 -0300

    Add files via upload

---
+++ user-documentation-7.0/docs/links/nano_kb_commands.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "Nano keyboard commands" 
+link_text = "Nano keyboard commands" 
+link_url = "https://www.nano-editor.org/dist/latest/cheatsheet.html" 
+
+link.xref_links.update({link_name: (link_text, link_url)})
